fb1993a0f45a893bfc34d7abbb2f39064f7a590c
malzzganz-cyber <retio1766@gmail.com>
2026-05-06 18:23:50 +0700

    Add setting.js

---
+++ setting.js
@@ -0,0 +1,3 @@
+// Konfigurasi global Malzz Pedia
+global.apikey = process.env.VPEDIA_API_KEY || 'KhafaTopUp_yjmm32s92qz3jn0d';
+global.feenya = parseInt(process.env.FEE || '250'); // Fee tambahan per transaksi
